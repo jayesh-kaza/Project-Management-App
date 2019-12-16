a8980a9163ffd5ef683e27b25970085ff6731ac8
rahul <rahulhopper047@gmail.com>
2019-12-16 11:50:33 +0530

    add home page

--- clientApp/src/App.js
+++ clientApp/src/App.js
@@ -4,14 +4,14 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import HomePageHeader from "./Components/Organism/HomePageHeader";
 
 const App = props => {
-  const [log, setLog] = useState(true);
-  const onClickHandle = () => {
-    console.log(log)
-    setLog(!log)
-  }
+  // const [log, setLog] = useState(true);
+  // const onClickHandle = () => {
+  //   console.log
+  //   setLog(!log)
+  // }
   return <Router>
-    {log}?
-    <LandingPage onChange={onClickHandle} />:
+    {/* {log}?
+    <LandingPage onChange={onClickHandle} />: */}
     <HomePageHeader />
   </Router>;
 };
