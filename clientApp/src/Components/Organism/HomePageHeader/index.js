954d95b5a5496e39aad1fc35ab0d8c432ffc14b0
Jayesh Kaza <jayesh.kaza@zemosolabs.com>
2019-12-16 14:16:48 +0530

    "correct imports"

--- clientApp/src/Components/Organism/HomePageHeader/index.js
+++ clientApp/src/Components/Organism/HomePageHeader/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
+import {IconButton,Typography, AppBar, Toolbar} from '@material-ui/core';
 import { fade, makeStyles } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
