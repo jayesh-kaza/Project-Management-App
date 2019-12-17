63c21123fdfc14e6bf7f01fed19d5156a9daab4b
Jayesh Kaza <jayesh.kaza@zemosolabs.com>
2019-12-17 11:12:38 +0530

    "prettify code"

--- clientApp/src/Components/Organism/UserMenu/index.js
+++ clientApp/src/Components/Organism/UserMenu/index.js
@@ -1,53 +1,44 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import CheckIcon from '@material-ui/icons/Check';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import CheckIcon from "@material-ui/icons/Check";
 const StyledMenu = withStyles({
   paper: {
-    border: '1px solid #d3d4d5',
-  },
+    border: "1px solid #d3d4d5"
+  }
 })(props => (
   <Menu
     elevation={0}
     getContentAnchorEl={null}
     anchorOrigin={{
-      vertical: 'bottom',
-      horizontal: 'center',
+      vertical: "bottom",
+      horizontal: "center"
     }}
     transformOrigin={{
-      vertical: 'top',
-      horizontal: 'center',
+      vertical: "top",
+      horizontal: "center"
     }}
     {...props}
   />
 ));
-const useStyles = makeStyles(theme => ({
-  Items: {
-    backgroundColor: 'linear-gradient(to right bottom, #430089, #82ffa1)',
-    "&:hover": {
-      backgroundColor: 'linear-gradient(to right bottom, #430089, #82ffa1)'
-    }
-  },
-}));
+
 const StyledMenuItem = withStyles(theme => ({
   root: {
-    '&:focus': {
+    "&:focus": {
       backgroundColor: theme.palette.primary.main,
 
-      '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
-        color: theme.palette.common.white,
-      },
-    },
-  },
+      "& .MuiListItemIcon-root, & .MuiListItemText-primary": {
+        color: theme.palette.common.white
+      }
+    }
+  }
 }))(MenuItem);
 
 export default function UserMenu(props) {
-  const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
   const handleClick = event => {
     setAnchorEl(event.currentTarget);
@@ -63,7 +54,7 @@ export default function UserMenu(props) {
         aria-controls="customized-menu"
         aria-haspopup="true"
         onClick={handleClick}
-        style={{ backgroundColor: 'transparent', outline: "0", }}
+        style={{ backgroundColor: "transparent", outline: "0" }}
         disableRipple="true"
       >
         {props.name}
@@ -75,30 +66,26 @@ export default function UserMenu(props) {
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <StyledMenuItem >
+        <StyledMenuItem>
           <ListItemIcon>
             <CheckIcon fontSize="small" />
           </ListItemIcon>
           <ListItemText primary="zemosolabs" />
         </StyledMenuItem>
         <StyledMenuItem>
-          <ListItemIcon>
-          </ListItemIcon>
+          <ListItemIcon></ListItemIcon>
           <ListItemText primary="Organisation Settings" />
         </StyledMenuItem>
         <StyledMenuItem>
-          <ListItemIcon>
-          </ListItemIcon>
+          <ListItemIcon></ListItemIcon>
           <ListItemText primary="more" />
         </StyledMenuItem>
         <StyledMenuItem>
-          <ListItemIcon>
-          </ListItemIcon>
+          <ListItemIcon></ListItemIcon>
           <ListItemText primary="my profile" />
         </StyledMenuItem>
         <StyledMenuItem>
-          <ListItemIcon>
-          </ListItemIcon>
+          <ListItemIcon></ListItemIcon>
           <ListItemText primary="log out" />
         </StyledMenuItem>
       </StyledMenu>
