fc53ca0db2a89cccea6dc207ed71cced6632d2af
rahul <rahulhopper047@gmail.com>
2019-12-18 20:09:34 +0530

    fix margin

--- clientApp/src/Components/Pages/HomePage/index.js
+++ clientApp/src/Components/Pages/HomePage/index.js
@@ -26,34 +26,36 @@ const useStyles = makeStyles({
     },
 });
 const HomePage = (props) => {
-    const card = <Card raised='true' />
+    const b=<Button >click</Button>
+
     const classes = useStyles();
     return (
-        <Card raised='true' className={classes.card}>
-            <CardContent>
-                <Typography className={classes.title} color="textSecondary" gutterBottom>
-                    Word of the Day
-          </Typography>
-                <Typography variant="h5" component="h2">
-                    be
+        <Card children={b} raised='true' />
+        // <Card raised='true' className={classes.card}>
+        //     <CardContent>
+        //         <Typography className={classes.title} color="textSecondary" gutterBottom>
+        //             Word of the Day
+        //   </Typography>
+        //         <Typography variant="h5" component="h2">
+        //             be
 
-                    nev
+        //             nev
 
-                    lent
-          </Typography>
-                <Typography className={classes.pos} color="textSecondary">
-                    adjective
-          </Typography>
-                <Typography variant="body2" component="p">
-                    well meaning and kindly.
-            <br />
-                    {'"a benevolent smile"'}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Learn More</Button>
-            </CardActions>
-        </Card>
+        //             lent
+        //   </Typography>
+        //         <Typography className={classes.pos} color="textSecondary">
+        //             adjective
+        //   </Typography>
+        //         <Typography variant="body2" component="p">
+        //             well meaning and kindly.
+        //     <br />
+        //             {'"a benevolent smile"'}
+        //         </Typography>
+        //     </CardContent>
+        //     <CardActions>
+        //         <Button size="small">Learn More</Button>
+        //     </CardActions>
+        // </Card>
     )
 }
 export default HomePage;
