21cbe91bab309571659a16cb42eca6d6ec6f544d
sreetej <sreetejreddy1998@gmail.com>
2019-12-20 12:52:28 +0530

    added borderradius

--- clientApp/src/Components/Molecules/CustomCard/Card.js
+++ clientApp/src/Components/Molecules/CustomCard/Card.js
@@ -6,14 +6,14 @@ import clsx from 'classnames'
 const colors=['#E362E3','#EA4E9D','#AA62E3']
 const useStyles = makeStyles(theme=>({
     root:{
-       marginBottom:theme.spacing(6),
-       marginTop:theme.spacing(6),
+       marginBottom:theme.spacing(8),
+       marginTop:theme.spacing(8),
        
         marginRight:theme.spacing(3)
     },
     margin:{
-        marginBottom:theme.spacing(6),
-        marginTop:theme.spacing(6),
+        marginBottom:theme.spacing(9),
+        marginTop:theme.spacing(9),
         marginLeft:theme.spacing(8),
         marginRight:theme.spacing(8)
     }
@@ -24,7 +24,7 @@ const Card=()=>{
     const classes = useStyles();
 return (
 
-        <Paper className={classes.root} style={{ backgroundColor:colors[Math.floor(Math.random() * Math.floor(3))]}} elevation={6}>           
+        <Paper className={classes.root} style={{ backgroundColor:colors[Math.floor(Math.random() * Math.floor(3))],borderRadius:'25px'}} elevation={6}>           
             <div className={classes.margin}>
             <AssignmentIcon style={{color:'white',background:"transparant"}} fontSize="large"/>
             </div>
