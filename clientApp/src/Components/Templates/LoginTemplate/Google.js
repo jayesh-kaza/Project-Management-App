72ba5b118fd1f8a79ff209c2b8be8476171cc128
sreetej <sreetejreddy1998@gmail.com>
2019-12-23 11:17:56 +0530

    auth0

--- clientApp/src/Components/Templates/LoginTemplate/Google.js
+++ clientApp/src/Components/Templates/LoginTemplate/Google.js
@@ -2,45 +2,32 @@ import React, { useEffect } from "react";
 import "../../../css/main.css";
 import "../../../css/util.css";
 import img from "../../Atoms/Icon/Icons/icon-google.png";
-import {Link} from "react-router-dom"
+import auth0 from 'auth0-js'
+import Button from "@material-ui/core/Button";
+const login =async()=>{
+  var webAuth = await new auth0.WebAuth({
+    domain:       'dev-vg-166vp.auth0.com',
+    clientID:     'vDQ6nvuNYboGOwuZNjFnJ6Wme3B7JRZI',
+    redirectUri:'http://localhost:3000/tasks',
+    responseType :'id_token token'
+  });
 
+  // Trigger login with google
+  webAuth.authorize({
+    connection: 'google-oauth2'
+  });
 
-const login=async (uri)=>{
-  const url="https://www.googleapis.com/oauth2/v4/token"
-  console.log(uri.slice(uri.indexOf("code=")+5,uri.indexOf("&scope")))
+} 
 
-  const response=await fetch(url,{
-    method:'POST',
-    headers:{
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body:{
-      code:uri.slice(uri.indexOf("code=")+5,uri.indexOf("&scope")),
-      client_id:'245595399236-o5gujqtbe35dr6hrljaocuprqnsk5ft5.apps.googleusercontent.com',
-      client_secret:'eXuacGpN8uHJPrANGeY54-5v',
-      redirect_uri:'http://localhost:3000',
-      grant_type:'authorization_code',
-    }
-  })
-console.log(response)
-}
 const Google = props => {
-  const clientId="245595399236-o5gujqtbe35dr6hrljaocuprqnsk5ft5.apps.googleusercontent.com"
-  const scope="https://www.googleapis.com/auth/userinfo.profile"
-  const redirectUri="http://localhost:3000"
-  const url=`https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&response_type=code&scope=${scope}&redirect_uri=${redirectUri}&access_type=offline`
 
-  useEffect(()=>{
-    if(window.location.search.includes("code=")){
-      login(window.location.search)
-    }
-  },[window.location.search])
+ 
   return (
     <div align="center">
-      <a href={url}  className="btn-google m-b-10">
+      <Button  className="btn-google m-b-10" onClick={login}>
         <img src={img} alt="GOOGLE" />
         Use Google Account
-      </a>
+      </Button>
     </div>
   );
 };
--- clientApp/src/index.js
+++ clientApp/src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import auth0 from 'auth0-js'
+const auth=async()=>{
+return await new auth0.WebAuth({
+    domain:       'dev-vg-166vp.auth0.com',
+    clientID:     'vDQ6nvuNYboGOwuZNjFnJ6Wme3B7JRZI',
+    redirectUri:'http://localhost:3000/tasks',
+    responseType :'id_token token'
+  });}
+
+const AuthContext=React.createContext({auth0:auth()})
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
